938116a256c0a202a7788d46a86b353bcdaab1f4
tamaravuj <tamara.vujasevic5@gmail.com>
2026-04-16 19:49:49 +0200

    vezbe 5

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -8,8 +8,24 @@ using System.Threading.Tasks;
 
 namespace OTS_Supermarket.Test
 {
+    [TestFixture]
     public class CartTest
     {
+        [Test]
+        public void AddOneToCart_ShouldAddItemToCart_Succes()
+        {
+            //ARRANGEz
+            Cart cart = new Cart();
+            cart.Size = 5;
+            Monitor monitor = new Monitor();
 
+            //ACT
+            cart.AddOneToCart(monitor);
+
+            //ASSERT
+            Assert.That(cart.Size, Is.EqualTo(6));
+            Assert.That(cart.Amount, Is.EqualTo(100));
+
+        }
     }
 }
